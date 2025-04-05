76d2f3d601b408951911eaeadc58d5d8da37d9ed
Douglas <darkyal.7@gmail.com>
2025-04-05 18:52:52 -0300

    feat: added attributes in ContactDTO

--- src/main/java/com/meetime/challenge/DTOs/ContactDTO.java
+++ src/main/java/com/meetime/challenge/DTOs/ContactDTO.java
@@ -1,4 +1,14 @@
 package com.meetime.challenge.DTOs;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContactDTO {
+    private String email;
+    private String firstname;
+    private String lastname;
 }
