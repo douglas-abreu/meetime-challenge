44f77e502836347055a9ef4926997a21f49c700d
Douglas <darkyal.7@gmail.com>
2025-04-05 01:40:02 -0300

    feat: added possible application and Hubspot properties

---
+++ src/main/java/com/meetime/challenge/config/HubspotProperties.java
@@ -0,0 +1,19 @@
+package com.meetime.challenge.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "hubspot")
+@Getter @Setter
+public class HubspotProperties {
+    private String clientId;
+    private String clientSecret;
+    private String redirectUri;
+    private String authorizeUrl;
+    private String tokenUrl;
+    private String scopes;
+    private String apiBaseUrl;
+}
