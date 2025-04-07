524068f92d569396c72feb1247b440e8b92ce87e
Douglas <darkyal.7@gmail.com>
2025-04-07 18:59:50 -0300

    feat: added at GlobalExceptionHandler a WebExchangeBindException Handlers

--- src/main/java/com/meetime/challenge/expection/GlobalExceptionHandler.java
+++ src/main/java/com/meetime/challenge/expection/GlobalExceptionHandler.java
@@ -1,15 +1,26 @@
 package com.meetime.challenge.expection;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
+@AllArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final MessageSource messageSource;
 
     @ExceptionHandler(WebClientResponseException.class)
     public ResponseEntity<ErrorResponse> handleWebClientException(WebClientResponseException ex) {
@@ -30,6 +41,24 @@ public class GlobalExceptionHandler {
                 HttpStatus.BAD_REQUEST,
                 "Bad Request",
                 ex.getMessage()
+        );
+
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<ErrorResponse> handle(WebExchangeBindException exception) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        List<String> errors = new ArrayList<>();
+        for (FieldError fieldError : fieldErrors) {
+            String messageError = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+            errors.add(messageError);
+        }
+
+        ErrorResponse error = ErrorResponse.from(
+                HttpStatus.BAD_REQUEST,
+                "Erro no formulário",
+                errors.toString()
         );
 
         return ResponseEntity.badRequest().body(error);
