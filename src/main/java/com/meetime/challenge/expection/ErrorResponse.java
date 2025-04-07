ba06ddf8cce590c393c4200fe6f57523bf6523a2
Douglas <darkyal.7@gmail.com>
2025-04-06 22:55:53 -0300

    refactor: removed at ErrorResponse the @getter

--- src/main/java/com/meetime/challenge/expection/ErrorResponse.java
+++ src/main/java/com/meetime/challenge/expection/ErrorResponse.java
@@ -1,9 +1,7 @@
 package com.meetime.challenge.expection;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
 public record ErrorResponse(int status, String error, String message) {
 
     public static ErrorResponse from(HttpStatus status, String error, String message) {
