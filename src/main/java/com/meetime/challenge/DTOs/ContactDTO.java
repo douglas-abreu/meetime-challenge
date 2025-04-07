1af940a5da1ce23c934c61d733770b244fc04356
Douglas <darkyal.7@gmail.com>
2025-04-07 18:55:26 -0300

    refactor: removed not necessary imports at ContactDTO

--- src/main/java/com/meetime/challenge/DTOs/ContactDTO.java
+++ src/main/java/com/meetime/challenge/DTOs/ContactDTO.java
@@ -2,13 +2,9 @@ package com.meetime.challenge.DTOs;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ContactDTO {
     @NotBlank(message = "Email é obrigatório")
     @Email(message = "Email precisa ser válido")
