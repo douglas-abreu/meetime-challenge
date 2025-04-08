16960c4f07c99d0fbc1390a6bc7e654173915618
Douglas <darkyal.7@gmail.com>
2025-04-07 22:33:28 -0300

    feat: validator at createContact RequestParam end-point

--- src/main/java/com/meetime/challenge/controller/ContactController.java
+++ src/main/java/com/meetime/challenge/controller/ContactController.java
@@ -1,15 +1,18 @@
 package com.meetime.challenge.controller;
 
-import com.meetime.challenge.DTOs.ContactDTO;
+import com.meetime.challenge.dto.ContactDTO;
 import com.meetime.challenge.service.ContactService;
+import com.meetime.challenge.validation.ValidBearerToken;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
 @AllArgsConstructor
+@Validated
 @RequestMapping("/contacts")
 public class ContactController {
 
@@ -17,7 +20,8 @@ public class ContactController {
 
     @PostMapping
     public ResponseEntity<Mono<String>> createContact(
-            @RequestBody @Valid ContactDTO request, @RequestHeader("Authorization") String token) {
+            @RequestBody @Valid ContactDTO request,
+            @RequestHeader("Authorization") @ValidBearerToken(message = "Token é obrigatório") String token) {
         Mono<String> result = service.createContact(request, token.replace("Bearer ", ""));
         return ResponseEntity.ok(result);
     }
